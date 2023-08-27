b85bfe2c49f4a44b0cf2767c525435a487a89b16
casperWein <19201040.casperweinberger@gmail.com>
2023-08-27 17:19:35 +0700

    supplier-svc

--- src/main/java/net/ellshadday/app/entity/bahanbaku/BahanBaku.java
+++ src/main/java/net/ellshadday/app/entity/bahanbaku/BahanBaku.java
@@ -5,14 +5,10 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
-
 @Data
-@Getter
-@Setter
 @Entity
 @Table(name = "bahanbaku")
 public class BahanBaku {
@@ -30,123 +26,23 @@ public class BahanBaku {
     private String ukuran;
     private int safety_stock;
     @CreatedDate
-    private Date createdAt;
+    private LocalDateTime createdAt;
     @LastModifiedDate
-    private Date updateAt;
+    private LocalDateTime updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "kategori_id", nullable = false)
     private KategoriBahanBaku kategori;
 
-    public String getKodebahan() {
-        return kodebahan;
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
 
-    public void setKodebahan(String kodebahan) {
-        this.kodebahan = kodebahan;
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
     }
 
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getWarna() {
-        return warna;
-    }
-
-    public void setWarna(String warna) {
-        this.warna = warna;
-    }
-
-    public String getGambar() {
-        return gambar;
-    }
-
-    public void setGambar(String gambar) {
-        this.gambar = gambar;
-    }
-
-    public long getRoll_ball_quantity() {
-        return roll_ball_quantity;
-    }
-
-    public void setRoll_ball_quantity(long roll_ball_quantity) {
-        this.roll_ball_quantity = roll_ball_quantity;
-    }
-
-    public float getTotal_yard_kg() {
-        return total_yard_kg;
-    }
-
-    public void setTotal_yard_kg(float total_yard_kg) {
-        this.total_yard_kg = total_yard_kg;
-    }
-
-    public float getYard_kg_clean() {
-        return yard_kg_clean;
-    }
-
-    public void setYard_kg_clean(float yard_kg_clean) {
-        this.yard_kg_clean = yard_kg_clean;
-    }
-
-    public float getYard_kg_sisa() {
-        return yard_kg_sisa;
-    }
-
-    public void setYard_kg_sisa(float yard_kg_sisa) {
-        this.yard_kg_sisa = yard_kg_sisa;
-    }
-
-    public String getSatuan() {
-        return satuan;
-    }
-
-    public void setSatuan(String satuan) {
-        this.satuan = satuan;
-    }
-
-    public String getUkuran() {
-        return ukuran;
-    }
-
-    public void setUkuran(String ukuran) {
-        this.ukuran = ukuran;
-    }
-
-    public int getSafety_stock() {
-        return safety_stock;
-    }
-
-    public void setSafety_stock(int safety_stock) {
-        this.safety_stock = safety_stock;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
-
-    public KategoriBahanBaku getKategori() {
-        return kategori;
-    }
-
-    public void setKategori(KategoriBahanBaku kategori) {
-        this.kategori = kategori;
-    }
 }
--- src/main/java/net/ellshadday/app/payload/bahanBakuDto/KategoriBahanBakuDto.java
+++ src/main/java/net/ellshadday/app/payload/bahanBakuDto/KategoriBahanBakuDto.java
@@ -7,28 +7,4 @@ public class KategoriBahanBakuDto {
     private long id;
     private String nama;
     private String keterangan;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getKeterangan() {
-        return keterangan;
-    }
-
-    public void setKeterangan(String keterangan) {
-        this.keterangan = keterangan;
-    }
 }
